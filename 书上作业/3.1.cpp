90e71c667e22258cc7009f4608f168bcf6d74c2a
Halry Smith <liangokdong@gmail.com>
2016-03-17 22:45:43 +0800

    fix

--- 书上作业/3.1.cpp
+++ 书上作业/3.1.cpp
@@ -1,13 +1,13 @@
 #include <stdafx.h>
 #include <Math.h>
 #define PI 3.14
-double radius, area, long;
+double radius, area, l1;
 int main()
 {
 	printf("Enter radius:");
 	scanf("%lf", &radius);
 		area = pow(radius, 2)*PI;
-		long=2*PI*radius;
-		printf("Area is %lf,long:%lf", area,long);
+		l1=2*PI*radius;
+		printf("Area is %lf,long:%lf", area,l1);
 	while (1);
 }
